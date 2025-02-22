e0e511b9afce7cd70ecba0a42c26709d1f88a42f
raul-quesada <raul.quesada@goumh.umh.es>
2025-02-22 14:07:48 +0100

    Modificación Test

--- p1-springboot-hello-world-app/src/test/java/demoapp/MockMvcTest.java
+++ p1-springboot-hello-world-app/src/test/java/demoapp/MockMvcTest.java
@@ -33,8 +33,8 @@ public class MockMvcTest {
     @Test
     public void postShoudReturnCorrectResponse() throws Exception {
         this.mockMvc.perform(post("/saludoform")
-                .param("nombre", "Juan"))
+                .param("nombre", "Raul"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello Juan")));
+                .andExpect(content().string(containsString("Hello Raul")));
     }
 }
