0855c06d840d5b181dfe9f7e24025c3715ab4fd8
raul-quesada <raul.quesada@goumh.umh.es>
2025-02-25 00:27:02 +0100

    Creacion de html hello que se lanza en el Home Controller, que incluye el hello World y una lista de opciones con que te manda a la URL ya creadas en los Controladores que ya estan creados (/saludorform, /saludo/{nomrbe}, /saludoplantilla/{nombre})

--- p1-springboot-hello-world-app/src/main/java/demoapp/controller/HomeController.java
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello World, my name is Raul";
+    public String greeting() {
+        return "hello";
     }
 }
